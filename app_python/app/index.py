19d2ea5359d3b534145e6309a88975276d0aee46
Marko Pezer <markopezer@gmail.com>
2021-09-27 20:46:03 +0200

    Update app

--- app_python/app/index.py
+++ app_python/app/index.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+import datetime
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
@@ -11,4 +12,13 @@ templates = Jinja2Templates(directory = "templates")
 @app.get("/time", response_class = HTMLResponse)
 
 async def read_item(request: Request):
+    with open('tmp_time.txt', 'a') as fp:
+		fp.write(str(datetime.datetime.now()) + '\n')
     return templates.TemplateResponse("current_time.html", {"request": request})
+
+@app.get("/visits", response_class = HTMLResponse)
+async def read_file(request: Request):
+	return '\n'.join(open('tmp_time.txt', 'r').readlines())
+
+
+
